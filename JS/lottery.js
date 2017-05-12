bb043dceaa90a16bc01a04f2450248175a5a3ae4
luxuriant825 <1509212147@qq.com>
2017-05-12 11:32:13 +0800

    Create lottery.js

---
+++ JS/lottery.js
@@ -0,0 +1,65 @@
+var flag=0;
+	window.onload=function(){
+		var move=document.getElementById('move');
+		var alis=move.getElementsByTagName('a');
+		for (var i=0;i<alis.length;i++)
+		{
+			alis[i].onmouseover=function()
+			{
+				var ths=this.getElementsByTagName('i')[0];
+				start(ths,{top:-25,opacity:0},function(){
+					ths.style.top=40+'px';
+					start(ths,{top:20,opacity:100});
+				});
+			}
+		}
+		
+		var start1=document.getElementById('start');
+		var stop1=document.getElementById('stop');
+		start1.onclick=sgame;
+		stop1.onclick=egame;
+		
+		document.onkeyup=function(event){
+			event=event||window.event;
+			if (event.keyCode==13)
+			{
+				if (flag==0){sgame();flag=1;}
+				else{egame();flag=0;}
+			}
+		
+		}
+		
+	}
+	var timer=null;
+	
+	function sgame(){
+		var pics=document.getElementsByTagName('a');
+		var start1=document.getElementById('start');
+		var stop1=document.getElementById('stop');
+		clearInterval(timer);
+		start1.style.background='#999';
+		var j=0;
+		var num=Math.floor(Math.random()*pics.length);
+		if (j<num){
+		timer=setInterval(function(){
+			for (var i=0;i<pics.length;i++)
+			{
+				pics[i].className='';
+			}
+			pics[j%pics.length].className='active';
+			j++;
+			
+		},130);
+		}
+	
+	}
+	
+	function egame(){
+		var pics=document.getElementsByTagName('a');
+		var start1=document.getElementById('start');
+		var stop1=document.getElementById('stop');
+		clearInterval(timer);
+		start1.style.background='#c63';
+	}
+	
+	
